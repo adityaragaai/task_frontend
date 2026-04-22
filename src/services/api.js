8f417777c96d7f7a280607f53baa275633ed6c51
aditya <aditya.gupta@raga.ai>
2026-04-22 15:02:57 +0530

    fixed issue

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://task-backend-qek0.onrender.com/api/tasks';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://task-backend-qek0.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
